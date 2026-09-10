e1b5e1593bac3351bd582fa027404f97e42d865f
SeongHyeok Kim <sunghyeck1779@gmail.com>
2026-09-10 15:01:17 +0900

    스크립트 업데이트 및 패키지 추가

--- run_dashboard.ps1
+++ run_dashboard.ps1
@@ -1,13 +1,2 @@
-./test.ps1
-
-if ($LASTEXITCODE -ne 0 -and $LASTEXITCODE -ne 5) {
-
-    Write-Host ""
-    Write-Host "Test failed."
-    exit $LASTEXITCODE
-}
-
-Write-Host ""
-Write-Host "Test success! Running Streamlit application.."
-
+conda env update -n kdt-project-1 -f environment.yml
 conda run -n kdt-project-1 streamlit run dashboard\app.py
